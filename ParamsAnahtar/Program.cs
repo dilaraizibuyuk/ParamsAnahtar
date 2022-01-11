a3de15fc794afb3d7e6a7dd49891a09848a2bc2a
Dilara <74327986+dilaraizibuyuk@users.noreply.github.com>
2022-01-11 19:44:25 +0300

    Proje dosyası ekle.

---
+++ ParamsAnahtar/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParamsAnahtar
+{
+    class Program
+    {
+        public static int SayilariTopla(params int[] sayilar)
+        {
+            int toplam = 0;
+            foreach (int i in sayilar)
+            {
+                toplam += i;
+            }
+            return toplam;
+        }
+        static void Main(string[] args)
+        {
+            int result1 = SayilariTopla(22);
+            int result2 = SayilariTopla(7, 5, -3);
+            int result3 = SayilariTopla(-8, 8, 7, 5, -7, 2, 9, 0);
+            Console.WriteLine("Sonuçlar:"+result1);
+            Console.WriteLine("Sonuçlar:" + result2);
+            Console.WriteLine("Sonuçlar:" + result3);
+            Console.ReadKey();
+
+        }
+    }
+}
